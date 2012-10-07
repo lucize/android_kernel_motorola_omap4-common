4f1238bd5f105e1a3ec598133cbc91f5e8a3a71d
Wei Yongjun <yongjun_wei@trendmicro.com.cn>
2012-10-08 02:44:47 +0800

    drivers:misc: ti-st: fix potential NULL pointer dereference in st_register()
    drivers:misc: ti-st: fix potential NULL pointer dereference in st_register()
    
    Remove the pointless NULL dereference above the NULL test.
    
    dpatch engine is used to auto generate this patch.
    (https://github.com/weiyj/dpatch)
    
    Signed-off-by: Wei Yongjun <yongjun_wei@trendmicro.com.cn>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Conflicts:
    	drivers/misc/ti-st/st_core.c

--- drivers/misc/ti-st/st_core.c
+++ drivers/misc/ti-st/st_core.c
@@ -520,7 +520,10 @@ long st_register(struct st_proto_s *new_proto)
 	unsigned long flags = 0;
 
 	st_kim_ref(&st_gdata, 0);
+<<<<<<< HEAD
 	pr_info("%s(%d)\n", __func__, new_proto->chnl_id);
+=======
+>>>>>>> 114239f... drivers:misc: ti-st: fix potential NULL pointer dereference in st_register()
 	if (st_gdata == NULL || new_proto == NULL || new_proto->recv == NULL
 	    || new_proto->reg_complete_cb == NULL) {
 		pr_err("gdata/new_proto/recv or reg_complete_cb not ready\n");
