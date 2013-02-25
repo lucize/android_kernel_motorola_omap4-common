07dde1f6da496b43a2c8cf65b4f86106af1c2231
Minsung Kim <ms925.kim@samsung.com>
2013-02-25 23:48:04 +0900

    cpufreq: interactive: allow arbitrary speed / delay mappings
    Accept a string of delays and speeds at which to apply the delay before
    raising each step above hispeed. For example, "80000 1300000:200000
    1500000:40000" means that the delay at or above 1GHz, until 1.3GHz is 80 msecs,
    the delay until 1.5GHz is 200 msecs and the delay at or above 1.5GHz is 40
    msecs when hispeed_freq is 1GHz.
    
    [toddpoynor@google.com: add documentation]
    Change-Id: Ifeebede8b1acbdd0a53e5c6916bccbf764dc854f
    Signed-off-by: Minsung Kim <ms925.kim@samsung.com>
    
    Conflicts:
    	drivers/cpufreq/cpufreq_interactive.c

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -127,7 +127,15 @@ static unsigned long default_timer_rate;
  * timer interval.
  */
 #define DEFAULT_ABOVE_HISPEED_DELAY DEFAULT_TIMER_RATE
+<<<<<<< HEAD
 static unsigned long above_hispeed_delay_val;
+=======
+static unsigned int default_above_hispeed_delay[] = {
+	DEFAULT_ABOVE_HISPEED_DELAY };
+static spinlock_t above_hispeed_delay_lock;
+static unsigned int *above_hispeed_delay = default_above_hispeed_delay;
+static int nabove_hispeed_delay = ARRAY_SIZE(default_above_hispeed_delay);
+>>>>>>> 0bda0b2... cpufreq: interactive: allow arbitrary speed / delay mappings
 
 /*
  * Boost pulse to hispeed on touchscreen input.
@@ -174,7 +182,28 @@ void cpufreq_interactive_set_timer_rate(unsigned long val, unsigned int reset)
 }
 #endif
 
+<<<<<<< HEAD
 static void cpufreq_interactive_timer(unsigned long data)
+=======
+static unsigned int freq_to_above_hispeed_delay(unsigned int freq)
+{
+	int i;
+	unsigned int ret;
+	unsigned long flags;
+
+	spin_lock_irqsave(&above_hispeed_delay_lock, flags);
+
+	for (i = 0; i < nabove_hispeed_delay - 1 &&
+			freq >= above_hispeed_delay[i+1]; i += 2)
+		;
+
+	ret = above_hispeed_delay[i];
+	spin_unlock_irqrestore(&above_hispeed_delay_lock, flags);
+	return ret;
+}
+
+static unsigned int freq_to_targetload(unsigned int freq)
+>>>>>>> 0bda0b2... cpufreq: interactive: allow arbitrary speed / delay mappings
 {
 	unsigned int delta_idle;
 	unsigned int delta_time;
@@ -324,7 +353,21 @@ static void cpufreq_interactive_timer(unsigned long data)
 			}
 		}
 	} else {
+<<<<<<< HEAD
 		new_freq = pcpu->policy->max * cpu_load / 100;
+=======
+		new_freq = choose_freq(pcpu, loadadjfreq);
+	}
+
+	if (pcpu->target_freq >= hispeed_freq &&
+	    new_freq > pcpu->target_freq &&
+	    now - pcpu->hispeed_validate_time <
+	    freq_to_above_hispeed_delay(pcpu->policy->cur)) {
+		trace_cpufreq_interactive_notyet(
+			data, cpu_load, pcpu->target_freq,
+			pcpu->policy->cur, new_freq);
+		goto rearm;
+>>>>>>> 0bda0b2... cpufreq: interactive: allow arbitrary speed / delay mappings
 	}
 
 	if (new_freq <= hispeed_freq)
@@ -710,7 +753,66 @@ static void cpufreq_interactive_input_event(struct input_handle *handle,
 	}
 }
 
+<<<<<<< HEAD
 static void cpufreq_interactive_input_open(struct work_struct *w)
+=======
+static struct notifier_block cpufreq_notifier_block = {
+	.notifier_call = cpufreq_interactive_notifier,
+};
+
+static unsigned int *get_tokenized_data(const char *buf, int *num_tokens)
+{
+	const char *cp;
+	int i;
+	int ntokens = 1;
+	unsigned int *tokenized_data;
+
+	cp = buf;
+	while ((cp = strpbrk(cp + 1, " :")))
+		ntokens++;
+
+	if (!(ntokens & 0x1)) {
+		tokenized_data = ERR_PTR(-EINVAL);
+		goto err;
+	}
+
+	tokenized_data = kmalloc(ntokens * sizeof(unsigned int), GFP_KERNEL);
+	if (!tokenized_data) {
+		tokenized_data = ERR_PTR(-ENOMEM);
+		goto err;
+	}
+
+	cp = buf;
+	i = 0;
+	while (i < ntokens) {
+		if (sscanf(cp, "%u", &tokenized_data[i++]) != 1) {
+			tokenized_data = ERR_PTR(-EINVAL);
+			goto err_kfree;
+		}
+
+		cp = strpbrk(cp, " :");
+		if (!cp)
+			break;
+		cp++;
+	}
+
+	if (i != ntokens) {
+		tokenized_data = ERR_PTR(-EINVAL);
+		goto err_kfree;
+	}
+
+	*num_tokens = ntokens;
+	return tokenized_data;
+
+err_kfree:
+	kfree(tokenized_data);
+err:
+	return tokenized_data;
+}
+
+static ssize_t show_target_loads(
+	struct kobject *kobj, struct attribute *attr, char *buf)
+>>>>>>> 0bda0b2... cpufreq: interactive: allow arbitrary speed / delay mappings
 {
 	struct cpufreq_interactive_inputopen *io =
 		container_of(w, struct cpufreq_interactive_inputopen,
@@ -726,6 +828,7 @@ static int cpufreq_interactive_input_connect(struct input_handler *handler,
 					     struct input_dev *dev,
 					     const struct input_device_id *id)
 {
+<<<<<<< HEAD
 	struct input_handle *handle;
 	int error;
 
@@ -755,6 +858,23 @@ static void cpufreq_interactive_input_disconnect(struct input_handle *handle)
 	input_close_device(handle);
 	input_unregister_handle(handle);
 	kfree(handle);
+=======
+	int ntokens;
+	unsigned int *new_target_loads = NULL;
+	unsigned long flags;
+
+	new_target_loads = get_tokenized_data(buf, &ntokens);
+	if (IS_ERR(new_target_loads))
+		return PTR_RET(new_target_loads);
+
+	spin_lock_irqsave(&target_loads_lock, flags);
+	if (target_loads != default_target_loads)
+		kfree(target_loads);
+	target_loads = new_target_loads;
+	ntarget_loads = ntokens;
+	spin_unlock_irqrestore(&target_loads_lock, flags);
+	return count;
+>>>>>>> 0bda0b2... cpufreq: interactive: allow arbitrary speed / delay mappings
 }
 
 static const struct input_device_id cpufreq_interactive_ids[] = {
@@ -783,6 +903,50 @@ static struct input_handler cpufreq_interactive_input_handler = {
 	.name           = "cpufreq_interactive",
 	.id_table       = cpufreq_interactive_ids,
 };
+
+static ssize_t show_above_hispeed_delay(
+	struct kobject *kobj, struct attribute *attr, char *buf)
+{
+	int i;
+	ssize_t ret = 0;
+	unsigned long flags;
+
+	spin_lock_irqsave(&above_hispeed_delay_lock, flags);
+
+	for (i = 0; i < nabove_hispeed_delay; i++)
+		ret += sprintf(buf + ret, "%u%s", above_hispeed_delay[i],
+			       i & 0x1 ? ":" : " ");
+
+	ret += sprintf(buf + ret, "\n");
+	spin_unlock_irqrestore(&above_hispeed_delay_lock, flags);
+	return ret;
+}
+
+static ssize_t store_above_hispeed_delay(
+	struct kobject *kobj, struct attribute *attr, const char *buf,
+	size_t count)
+{
+	int ntokens;
+	unsigned int *new_above_hispeed_delay = NULL;
+	unsigned long flags;
+
+	new_above_hispeed_delay = get_tokenized_data(buf, &ntokens);
+	if (IS_ERR(new_above_hispeed_delay))
+		return PTR_RET(new_above_hispeed_delay);
+
+	spin_lock_irqsave(&above_hispeed_delay_lock, flags);
+	if (above_hispeed_delay != default_above_hispeed_delay)
+		kfree(above_hispeed_delay);
+	above_hispeed_delay = new_above_hispeed_delay;
+	nabove_hispeed_delay = ntokens;
+	spin_unlock_irqrestore(&above_hispeed_delay_lock, flags);
+	return count;
+
+}
+
+static struct global_attr above_hispeed_delay_attr =
+	__ATTR(above_hispeed_delay, S_IRUGO | S_IWUSR,
+		show_above_hispeed_delay, store_above_hispeed_delay);
 
 static ssize_t show_hispeed_freq(struct kobject *kobj,
 				 struct attribute *attr, char *buf)
@@ -851,28 +1015,6 @@ static ssize_t store_min_sample_time(struct kobject *kobj,
 
 static struct global_attr min_sample_time_attr = __ATTR(min_sample_time, 0644,
 		show_min_sample_time, store_min_sample_time);
-
-static ssize_t show_above_hispeed_delay(struct kobject *kobj,
-					struct attribute *attr, char *buf)
-{
-	return sprintf(buf, "%lu\n", above_hispeed_delay_val);
-}
-
-static ssize_t store_above_hispeed_delay(struct kobject *kobj,
-					 struct attribute *attr,
-					 const char *buf, size_t count)
-{
-	int ret;
-	unsigned long val;
-
-	ret = strict_strtoul(buf, 0, &val);
-	if (ret < 0)
-		return ret;
-	above_hispeed_delay_val = val;
-	return count;
-}
-
-define_one_global_rw(above_hispeed_delay);
 
 static ssize_t show_timer_rate(struct kobject *kobj,
 			struct attribute *attr, char *buf)
@@ -1101,9 +1243,13 @@ static struct global_attr low_power_rate_attr = __ATTR(low_power_rate,
 
 
 static struct attribute *interactive_attributes[] = {
+<<<<<<< HEAD
+=======
+	&target_loads_attr.attr,
+	&above_hispeed_delay_attr.attr,
+>>>>>>> 0bda0b2... cpufreq: interactive: allow arbitrary speed / delay mappings
 	&hispeed_freq_attr.attr,
 	&go_hispeed_load_attr.attr,
-	&above_hispeed_delay.attr,
 	&min_sample_time_attr.attr,
 	&timer_rate_attr.attr,
 	&input_boost.attr,
